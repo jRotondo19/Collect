11efe71f8717d7e7cc89c0679e91561072b239b7
jRotondo19 <59121470+jRotondo19@users.noreply.github.com>
2020-11-04 16:54:10 -0600

    Add method to count unique set

--- Collect.java
+++ Collect.java
@@ -1,4 +1,14 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Collect {
 
+	public static void main (String[] args) {
+	
+	List<String> collections = Arrays.asList("hello", "java is fun", "hello");
+	Set<String> uniqueCollections = new HashSet<String>(collections);
+	System.out.println("Unique collections: " + uniqueCollections.size());
+	}
 }
